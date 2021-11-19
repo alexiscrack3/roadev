3384a5d53eeb08413c188449f0a78a78bc208707
Alexis Ortega <alexis.oc3@gmail.com>
2021-11-19 15:21:44 -0700

    Add learning path model

---
+++ app/models/learning_path.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: learning_paths
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class LearningPath < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+LearningPath.delete_all
+LearningPath.create!(
+  title: 'Frontend',
+  description: 'Step by step guide to becoming a frontend developer'
+)
+
+LearningPath.create!(
+  title: 'Backend',
+  description: 'Step by step guide to becoming a backend developer'
+)
+
+LearningPath.create!(
+  title: 'DevOps',
+  description: 'Step by step guide for DevOps or operations role'
+)
+
+6.times do
+  LearningPath.create!(
+    title: Faker::Name.unique.name,
+    description: Faker::Lorem.sentence
+  )
+end
