1ef3afc886bf0e2c8405c06c75ab37755ddea743
Alexis Ortega <alexis.oc3@gmail.com>
2021-11-20 22:57:25 -0700

    Add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/dashboard', to: 'dashboard#index'
+  get '/users/sign_up', to: 'sessions#sign_up'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
