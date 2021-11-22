15870f63cdf55b33c9f563fd079bc0fa4e7e3eca
Alexis Ortega <alexis.oc3@gmail.com>
2021-11-21 21:40:47 -0700

    Create custom task to run tests with coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
-require 'simplecov'
-SimpleCov.start
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
