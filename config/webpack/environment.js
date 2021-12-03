0a18df2c90944eb5438bd13e00a2895acce0f32f
Alexis Ortega <alexis.oc3@gmail.com>
2021-12-02 19:31:01 -0700

    Load jQuery plugin correctly

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,8 +3,9 @@ const webpack = require("webpack");
 
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        // 'window.jQuery': 'jquery/src/jquery',
         Popper: ['popper.js', 'default']
     })
 );
