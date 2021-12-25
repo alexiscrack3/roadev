7c53b4b97e7eb93bf03c2f2c300e7f2ba36e0a68
Alexis Ortega <alexis.oc3@gmail.com>
2021-12-25 13:56:51 -0800

    Allow users to reset their password

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :account_activations, only: [:edit]
   resources :learning_paths, only: [:show]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
