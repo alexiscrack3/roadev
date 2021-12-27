05a2c8fae2f424ea8957b6c4f49bef9d53c620eb
Alexis Ortega <alexis.oc3@gmail.com>
2021-12-27 10:45:15 -0800

    Add default_url_options for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "roadev_production"
+  config.action_mailer.default_url_options = { host: ENV["HOST"], protocol: "http" }
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
