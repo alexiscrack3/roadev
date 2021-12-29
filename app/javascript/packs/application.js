83a5ffec277ee97cf2f0a663bf7814ad8d684c21
Alexis Ortega <alexis.oc3@gmail.com>
2021-12-29 08:56:13 -0800

    Add bootstrap-icons package

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import * as Bootstrap from "bootstrap"
-import "../stylesheets/application"
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 Rails.start()
 Turbolinks.start()
