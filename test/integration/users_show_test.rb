bbe55061f0fa3131df984446cdab4e38d73e5d91
Alexis Ortega <alexis.oc3@gmail.com>
2022-01-26 15:58:33 -0700

    Redirect user to sign-in page if user is not authenticate while navigating to profile page

--- test/integration/users_show_test.rb
+++ test/integration/users_show_test.rb
@@ -1,14 +1,13 @@
 require "test_helper"
 
 class UsersShowTest < ActionDispatch::IntegrationTest
-  # def setup
-  #   @user = users(:user_a)
-  # end
+  def setup
+    @user_a = users(:user_a)
+  end
 
-  # test "user info should be displayed" do
-  #   log_in_as(@user)
-  #   get user_path(@user)
-  #   assert_template "users/show"
-  #   assert_select "p", text: "First name: #{@user.first_name}"
-  # end
+  test "user should be redirected to sign-in page when user is not authenticated" do
+    get user_path(@user_a)
+    assert_not flash.empty?
+    assert_redirected_to sign_in_path
+  end
 end
