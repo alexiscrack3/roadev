2377b1d488710d0e7f78edac90fb897900dc2c5e
Alexis Ortega <alexis.oc3@gmail.com>
2022-01-25 23:58:35 -0700

    Remove bootstrap variable

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "bootstrap"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
