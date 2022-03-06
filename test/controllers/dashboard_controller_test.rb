7d70bf78d9a54d41708c3d05048c6c3fbfa22750
Alexis Ortega <alexis.oc3@gmail.com>
2022-03-05 20:55:36 -0700

    Check number of learning paths on dashboard

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -5,4 +5,10 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     get dashboard_path
     assert_response :success
   end
+
+  test "learning path should be displayed" do
+    get dashboard_path
+    learning_paths = assigns(:learning_paths)
+    assert learning_paths.length == 3
+  end
 end
